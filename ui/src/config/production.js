7d9da3b1b351f552f9eeb08e76c1c10d684de7d9
Ross Mahony <rmahony@gmail.com>
2017-12-16 23:39:05 +0000

    updated production configuration

--- ui/src/config/production.js
+++ ui/src/config/production.js
@@ -1,10 +1,8 @@
 /* Production environment configuration file. Replace placeholders with environment specific configuration */
 module.exports = {
   FIREBASE: {
-    API_KEY: '<API_KEY>',
-    PROJECT_ID: '<PROJECT_ID>',
-    DATABASE_NAME: '<DATABASE_NAME>',
-    BUCKET: '<BUCKET>',
-    SENDER_ID: '<SENDER>'
+    API_KEY: 'AIzaSyCag1zGLmDMZuavN7miblGtf56GzwPiCZo',
+    PROJECT_ID: 'storytellers2-13997',
+    SENDER_ID: '1039632406698'
   }
 }
